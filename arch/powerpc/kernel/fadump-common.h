13a4cc20d6c6cfa811698083777b67619dd4bb2e
Hari Bathini <hbathini@linux.ibm.com>
2019-08-20 17:34:50 +0530

    powerpc/fadump: Define register/un-register callback functions
    Make RTAS calls to register and un-register for FADump. Also, update
    how fadump_region contents are diplayed to provide more information.
    
    Signed-off-by: Hari Bathini <hbathini@linux.ibm.com>
    Reviewed-by: Mahesh Salgaonkar <mahesh@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/156630269035.8896.2265146808387132419.stgit@hbathini.in.ibm.com

--- arch/powerpc/kernel/fadump-common.h
+++ arch/powerpc/kernel/fadump-common.h
@@ -92,6 +92,8 @@ struct fw_dump {
 
 	unsigned long	cpu_state_data_size;
 	unsigned long	hpte_region_size;
+
+	unsigned long	boot_mem_dest_addr;
 	unsigned long	boot_memory_size;
 
 	unsigned long	fadumphdr_addr;
